b106a09ac6ef361ed94cc29c72ecafa486700612
tapekrol <46072098+tapekrol@users.noreply.github.com>
2019-01-06 19:46:38 +0300

    init commit

---
+++ GothBoiClique/script.js
@@ -0,0 +1,8 @@
+var canvas = document.getElementById("myCanvas");
+  context = canvas.getContext("2d");
+  
+var canvas = document.getElementById("myCanvas"), 
+    context = canvas.getContext("2d");
+ 
+context.fillRect(50, 40, 100, 100);
+context.clearRect(50, 40, 40, 40);
